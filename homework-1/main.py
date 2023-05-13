025b70ea2e4a2b58fca117f2d68c820b1f105ba9
Veronika <veronika.apakova@mail.ru>
2023-05-13 11:32:12 +0300

    refactor code

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,7 +2,7 @@ from src.channel import Channel
 
 if __name__ == '__main__':
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-    vdud.print_info()
+    print(vdud.get_info())
 
     """
 {
--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 from googleapiclient.discovery import build
 from dotenv import load_dotenv, find_dotenv
 
@@ -7,9 +7,6 @@ from helper.youtube_api_manual import youtube
 
 load_dotenv(find_dotenv())
 api_key: str = os.getenv('YT_API_KEY')
-
-
-
 
 
 class Channel:
@@ -20,11 +17,7 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def get_info(self) -> str:
         """Выводит в консоль информацию о канале."""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        return channel
-
-vdud = Channel("UCMCgOm8GZkHp8zJ6l7_hIuA")
-
-print(vdud.print_info())
+        return json.dumps(channel)
